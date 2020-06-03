f077452c8f5cf425782f3191042cc74d8c1b5968
Ruslan Bredikhin <rus.brn@gmail.com>
2020-06-03 08:16:09 -0400

    fix the tests

--- src/CompoundInterest.test.tsx
+++ src/CompoundInterest.test.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, render } from 'enzyme';
-import CompoundInterest from './CompoundInterest';
+import CompoundInterest, { compound } from './CompoundInterest';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const sample = {
+  input: {
+    principal: 100,
+    interest: 10,
+    annualAddition: 100,
+    timesPerYear: 12,
+    numberOfYears: 10
+  },
+  result: 199197
+};
+
 describe('Compound Interest Calculator', () => {
+  it('should calculate the result properly', () => {
+    expect(Math.round(compound(sample.input) * 100)).toEqual(sample.result);
+  });
+
   it('should render without props', () => {
     expect(shallow(<CompoundInterest />)).toMatchSnapshot();
   });
--- src/stories/CompoundInterest.stories.js
+++ src/stories/CompoundInterest.stories.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import CompoundInterest from '../';
+import React from 'react'
+import CompoundInterest from '../'
 
 export default {
   component: CompoundInterest,
   title: 'CompoundInterest',
-};
+}
 
-export const withDefaults = () => <CompoundInterest />;
-export const withTitle = () => <CompoundInterest title="Compound Interest Calculator" />;
-export const withoutAnnualAddition = () => <CompoundInterest options={{ withAnnualAddition: false}} />;
+export const withDefaults = () => <CompoundInterest />
+export const withTitle = () => (
+  <CompoundInterest title='Compound Interest Calculator' />
+)
+export const withoutAnnualAddition = () => (
+  <CompoundInterest options={{ withAnnualAddition: false }} />
+)
